e3d2bf9e0a5651747f18783abe7eaf732c68c32a
testmen <leo.bigot14@gmail.com>
2026-01-11 16:54:43 +0100

    First commit

---
+++ modules/home/packages/dev.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # env
+    devenv
+
+    ## Lsp
+    nixd # nix
+
+    ## formating
+    shfmt
+    treefmt
+    nixfmt
+
+    ## C / C++
+    gcc
+    gdb
+    gef
+    cmake
+    gnumake
+    valgrind
+    llvmPackages_20.clang-tools
+
+    ## Python
+    python3
+    python312Packages.ipython
+  ];
+}
